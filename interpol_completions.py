2871eb5875a665f817f82e66d156d262615d52cb
Thomas Bradford <thom@kode4food.it>
2015-02-05 08:35:38 +0100

    list of simple tags

--- interpol_completions.py
+++ interpol_completions.py
@@ -1,5 +1,37 @@
 import sublime, sublime_plugin
 
+interpol_simple_tags = [
+  "abbr", "acronym", "address", "applet", "area", "article", "aside", "audio",
+  "base", "basefont", "bdi", "bdo", "big", "blockquote", "body", "br",
+  "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup",
+  "datalist", "dd", "del", "details", "dfn", "dialog", "dir", "div", "dl",
+  "dt", "em", "embed", "fieldset", "figcaption", "figure", "font", "frame",
+  "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup",
+  "hr", "html", "input", "ins", "kbd", "keygen", "label", "legend",
+  "li", "main", "map", "mark", "menu", "menuitem", "meta", "meter", "nav",
+  "noframes", "noscript", "object", "ol", "optgroup", "option", "output",
+  "pre", "progress", "rp", "rt", "ruby", "samp", "section", "select", "small",
+  "source", "span", "strike", "strong", "sub", "summary", "sup", "table",
+  "tbody", "td", "textarea", "tfoot", "th", "thead", "time", "title", "tr",
+  "track", "tt", "ul", "var", "video", "wbr"
+]
+
+interpol_completion_items = [
+    ("a", "a href=\"$1\">$2</a>"),
+    ("iframe", "iframe src=\"$1\"></iframe>"),
+    ("img", "img src=\"$1\">"),
+    ("link", "link rel=\"stylesheet\" type=\"text/css\" href=\"$1\">"),
+    ("param", "param name=\"$1\" value=\"$2\">"),
+    ("script", "script type=\"${1:text/javascript}\">$0</script>"),
+    ("style", "style type=\"${1:text/css}\">$0</style>")
+]
+
+interpol_completion_items += [(item, item + ">$1<" + item + ">") for item in interpol_simple_tags]
+interpol_completion_items = sorted(interpol_completion_items, key=lambda item: item[0])
+
+interpol_completion_tags = set([item[0] for item in interpol_completion_items])
+interpol_completion_list = [(item[0] + "\tTag", item[1]) for item in interpol_completion_items]
+
 class InterpolCompletions(sublime_plugin.EventListener):
     def on_query_completions(self, view, prefix, locations):
         if not view.match_selector(locations[0], "source.interpol"):
@@ -10,15 +42,14 @@ class InterpolCompletions(sublime_plugin.EventListener):
         if ch != '<':
             return []
 
-        return ([
-            ("a\tTag", "a href=\"$1\">$2</a>"),
-            ("iframe\tTag", "iframe src=\"$1\"></iframe>"),
-            ("link\tTag", "link rel=\"stylesheet\" type=\"text/css\" href=\"$1\">"),
-            ("script\tTag", "script type=\"${1:text/javascript}\">$0</script>"),
-            ("style\tTag", "style type=\"${1:text/css}\">$0</style>"),
+        result = list(interpol_completion_list)
+        if prefix not in interpol_completion_tags:
+            result.append(            
+                (prefix + "\tTag", prefix + ">$1</" + prefix + ">")
+            )
 
-            ("img\tTag", "img src=\"$1\">"),
-            ("param\tTag", "param name=\"$1\" value=\"$2\">"),
-
-            (prefix + "\tTag", prefix + ">$1</" + prefix + ">")
-        ], sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
+        return (
+            result, 
+            sublime.INHIBIT_WORD_COMPLETIONS | 
+                sublime.INHIBIT_EXPLICIT_COMPLETIONS
+        )
